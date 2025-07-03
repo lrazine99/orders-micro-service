dfe62ae51935bc52185daaf4d88fd0fbe4f1e46b
LouafiRazine <louafi.razine@gmail.com>
2025-07-03 15:09:59 +0200

    first commit

---
+++ src/test/java/com/projet_micro_service/orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet_micro_service.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
